ca162d5e5c0c35cbf4c10283958b8ee7f1daaf25
Fred <120761782+Fredstorm@users.noreply.github.com>
2022-12-19 05:05:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello user. This program will make you a dream band name.")
+city = input("Firstly, what is the name of the city that you grew up in? ")
+pet = input("Secondly, what is the name of your current or latest pet? ")
+print("Your dream band name is " + city + " " + pet + "!")
